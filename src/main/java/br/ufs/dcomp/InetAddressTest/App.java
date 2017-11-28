55ea7d7b559cc06650fe4d1402b5d84751985c9f
Tarcisio <tarcisio@dcomp.ufs.br>
2017-11-28 18:11:23 +0000

    primeira versao

---
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -0,0 +1,24 @@
+package br.ufs.dcomp.InetAddressTest;
+
+import java.net.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try{
+            
+            String name = "www.ufs.br";
+            InetAddress address = InetAddress.getByName(name); 
+            System.out.println( "Name:      "+ name);
+            System.out.println( "Address:   "+ address);
+
+        } catch (UnknownHostException e){
+            System.out.println(e.getMessage());
+        }                
+    }
+}
