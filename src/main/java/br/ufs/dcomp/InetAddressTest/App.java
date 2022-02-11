6eb0e996d7966e49b53e1205787c8a66c19ddab0
Ubuntu <ubuntu@ip-172-31-29-154.ec2.internal>
2022-02-11 23:29:02 +0000

    Update name

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -12,7 +12,7 @@ public class App
     {
         try{
             
-            String name = "www.uol.com.br";
+            String name = "www.ufs.br";
             InetAddress address = InetAddress.getByName(name); 
             System.out.println( "Name:      "+ name);
             System.out.println( "Address:   "+ address.getHostAddress());
