8fcea38c085c1105bb22015494c6d6d11f221919
Ubuntu <ubuntu@ip-172-31-38-11.ec2.internal>
2023-01-19 20:10:12 +0000

    Atualizado para getAllByName

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -12,11 +12,12 @@ public class App
     {
         try{
             
-            String name = "www.ufs.br";
-            InetAddress address = InetAddress.getByName(name); 
+            String name = "google.com";
+            InetAddress address[] = InetAddress.getAllByName(name); 
             System.out.println( "Name:      "+ name);
-            System.out.println( "Address:   "+ address.getHostAddress());
-
+            for (int i = 0; i < address.length; i++){
+               System.out.println( "Address:   "+ address[i].getHostAddress());
+            }  
         } catch (UnknownHostException e){
             System.out.println(e.getMessage());
         }                
