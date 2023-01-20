dd1ae95c36b4256d1cc6d24980af4fe645c9c8f7
Ubuntu <ubuntu@ip-172-31-38-11.ec2.internal>
2023-01-20 22:18:07 +0000

    update

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -12,12 +12,10 @@ public class App
     {
         try{
             
-            String name = "google.com";
-            InetAddress address[] = InetAddress.getAllByName(name); 
+            String name = "www.ufs.br";
+            InetAddress address = InetAddress.getByName(name); 
             System.out.println( "Name:      "+ name);
-            for (int i = 0; i < address.length; i++){
-               System.out.println( "Address:   "+ address[i].getHostAddress());
-            }  
+            System.out.println( "Address:   "+ address.getHostAddress());
         } catch (UnknownHostException e){
             System.out.println(e.getMessage());
         }                
