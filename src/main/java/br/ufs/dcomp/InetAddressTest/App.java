b8a2c91c691d8c1edb971bf6301df8440475896f
Ubuntu <ubuntu@ip-172-31-44-123.ec2.internal>
2025-10-09 00:23:02 +0000

    alteracao do nome do site

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -12,7 +12,7 @@ public class App
     {
         try{
             
-            String name = "google.com.br";
+            String name = "ufs.br";
             InetAddress address = InetAddress.getByName(name); 
             System.out.println( "Name:      "+ name);
             System.out.println( "Address:   "+ address.getHostAddress());
